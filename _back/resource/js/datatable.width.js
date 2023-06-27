167a004ff5231ca5be05fe346661c7672363a9c5
Frontlab-kr <contact@frontlab.kr>
2023-06-27 09:57:56 +0900

    Revert "2023 버전"
    This reverts commit 758c4d28ec1113a37a21953f0d443375edd2799b.

--- _back/resource/js/datatable.width.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-
-    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-        $.fn.dataTable.tables( {visible: true, api: true} ).columns.adjust();
-    });
-    $('.modal').on('shown.bs.modal', function (e) {
-        $.fn.dataTable.tables( {visible: true, api: true} ).columns.adjust();
-    });
-});
--- _back/resource/js/web/com/menu.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    let menuInit = function () {
-        let $menuWrap = $(".side-nav.metismenu");
-        $menuWrap.metisMenu({
-            toggle: false
-        });
-    }
-});
